e81d72e101c4f7cfbb524f90b9b15cb1fa863efb
Marcel Reinmuth <marcel.reinmuth@stud.uni-heidelberg.de>
2023-06-25 23:42:22 +0200

    add missing libs

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
-install.packages(c("ggplot2","rjson","mapedit"))
+install.packages(c("ggplot2","rjson","mapedit", "googlePolylines"))
 
 devtools::install_github("GIScience/openrouteservice-r")
+devtools::install_github("isciences/exactextract")
